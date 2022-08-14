b1eac5f630c08013fb0bd33c7c61319fe94d2302
Yunus Emre Sayın <87127328+YunusEmreSayin@users.noreply.github.com>
2022-08-14 16:56:20 +0300

    Add files via upload

---
+++ flappybird/flappybird/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace flappybird
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.kus = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.altengel = new System.Windows.Forms.PictureBox();
+            this.ustengel = new System.Windows.Forms.PictureBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.lblpuan = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.kus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altengel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ustengel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // kus
+            // 
+            this.kus.Image = ((System.Drawing.Image)(resources.GetObject("kus.Image")));
+            this.kus.Location = new System.Drawing.Point(38, 196);
+            this.kus.Name = "kus";
+            this.kus.Size = new System.Drawing.Size(39, 30);
+            this.kus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.kus.TabIndex = 0;
+            this.kus.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // altengel
+            // 
+            this.altengel.Image = ((System.Drawing.Image)(resources.GetObject("altengel.Image")));
+            this.altengel.Location = new System.Drawing.Point(130, 265);
+            this.altengel.Name = "altengel";
+            this.altengel.Size = new System.Drawing.Size(66, 122);
+            this.altengel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.altengel.TabIndex = 1;
+            this.altengel.TabStop = false;
+            this.altengel.Visible = false;
+            // 
+            // ustengel
+            // 
+            this.ustengel.Image = ((System.Drawing.Image)(resources.GetObject("ustengel.Image")));
+            this.ustengel.Location = new System.Drawing.Point(130, 12);
+            this.ustengel.Name = "ustengel";
+            this.ustengel.Size = new System.Drawing.Size(66, 130);
+            this.ustengel.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ustengel.TabIndex = 2;
+            this.ustengel.TabStop = false;
+            this.ustengel.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 3000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // lblpuan
+            // 
+            this.lblpuan.AutoSize = true;
+            this.lblpuan.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblpuan.Location = new System.Drawing.Point(104, 11);
+            this.lblpuan.Name = "lblpuan";
+            this.lblpuan.Size = new System.Drawing.Size(0, 18);
+            this.lblpuan.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(95, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 18);
+            this.label1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(350, 85);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SkyBlue;
+            this.ClientSize = new System.Drawing.Size(684, 389);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblpuan);
+            this.Controls.Add(this.ustengel);
+            this.Controls.Add(this.altengel);
+            this.Controls.Add(this.kus);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.kus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.altengel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ustengel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox kus;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox altengel;
+        private System.Windows.Forms.PictureBox ustengel;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label lblpuan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ flappybird/flappybird/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace flappybird
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //vars
+        int deger = 0; int sec = 0;int puan = 0; int boyutsec = 0;
+        Random rnd = new Random();
+        bool engelvar = false;
+
+        //events
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            kus.Top = kus.Top + deger;
+            if (engelvar == false)
+            {
+                engelyokfunc();
+            }
+            if (engelvar == true)
+            {
+                engelvarfunc();
+            }
+            if (kus.Bounds.IntersectsWith(altengel.Bounds) || kus.Bounds.IntersectsWith(ustengel.Bounds) || kus.Location.Y <= 1 || kus.Location.Y >= 428)
+            {
+                temas();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) deger = -5;
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Space) deger = 5;
+        }
+        //funcs
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            //this.BackgroundImage = pictureBox1.Image;
+            //MessageBox.Show("OYUN BİTTİ " + "PUAN: " + puan);
+            //this.Close();
+        }
+        public void temas()
+        {
+            timer1.Enabled = false;
+            // timer2.Enabled = true;
+            kus.Visible = false;
+            altengel.Visible = false;
+            ustengel.Visible = false;
+            this.BackgroundImage = pictureBox1.Image;
+            MessageBox.Show("PUAN: " + puan);
+            this.Close();
+        }
+        public void engelvarfunc()
+        {
+            if (sec == 1)
+            {
+                ustengel.Left = ustengel.Left - 5;
+            }
+            if (sec == 2)
+            {
+                altengel.Left = altengel.Left - 5;
+            }
+            if ((ustengel.Left < -100) || (altengel.Left < -100))
+            {
+                puan++;
+                lblpuan.Text = puan.ToString();
+                engelvar = false;
+                altengel.Left = 700;
+                ustengel.Left = 700;
+                ustengel.Visible = false;
+                altengel.Visible = false;
+            }
+        }
+        public void engelyokfunc()
+        {
+            engelvar = true;
+            sec = rnd.Next(1, 3);
+            if (sec == 1)
+            {
+                ustengel.Visible = true;
+                boyutsec = rnd.Next(1, 4);
+                if (boyutsec == 1) ustengel.Size = new Size(66, 130);
+                else if (boyutsec == 2) ustengel.Size = new Size(66, 160);
+                else if (boyutsec == 3) ustengel.Size = new Size(66, 190);
+            }
+            if (sec == 2)
+            {
+                altengel.Visible = true;
+                boyutsec = rnd.Next(1, 4);
+                if (boyutsec == 1) altengel.Size = new Size(66, 130);
+                else if (boyutsec == 2) altengel.Size = new Size(66, 160);
+                else if (boyutsec == 3) altengel.Size = new Size(66, 190);
+            }
+        }
+    }
+}
